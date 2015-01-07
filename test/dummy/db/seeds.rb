0d6348a5507c748e368030af3587b964599441f7
genta <genta@hawaii.edu>
2015-01-06 19:20:03 -1000

    Merge branch 'master' of https://github.com/uhm-coe/kapa
    Conflicts:
    	app/views/course/_registration_form.html.erb
    	app/views/course/_table.html.erb
    	app/views/course/registrations/show.html.erb
    	app/views/course/rosters/show.html.erb

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -19,8 +19,8 @@ ActiveRecord::Fixtures.create_fixtures(fixture_path, "curriculums")
 ActiveRecord::Fixtures.create_fixtures(fixture_path, "advising_sessions")
 ActiveRecord::Fixtures.create_fixtures(fixture_path, "transition_points")
 ActiveRecord::Fixtures.create_fixtures(fixture_path, "transition_actions")
-ActiveRecord::Fixtures.create_fixtures(fixture_path, "assessment_courses")
-ActiveRecord::Fixtures.create_fixtures(fixture_path, "assessment_course_registrations")
+ActiveRecord::Fixtures.create_fixtures(fixture_path, "courses")
+ActiveRecord::Fixtures.create_fixtures(fixture_path, "registrations")
 
 [Curriculum, TransitionPoint, AssessmentCourse].each do |c|
   c.update_all("academic_period = #{201610}")
