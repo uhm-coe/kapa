41ab63cf6c638b7e52ca8b7545091406e2f9132f
Marifel <mrbarbasa@gmail.com>
2015-02-13 12:33:55 -1000

    Implemented new data filtering method for users and user timestamps

--- app/models/user_timestamp.rb
+++ app/models/user_timestamp.rb
@@ -1,4 +1,11 @@
 class UserTimestamp < ActiveRecord::Base
   self.table_name = :timestamps
   belongs_to :user
+
+  def self.search(filter, options = {})
+    user_timestamps = UserTimestamp.includes([:user])
+    user_timestamps = user_timestamps.where("date(convert_tz(created_at, '+00:00', '-10:00')) >= ?", filter.date_start) if filter.date_start.present?
+    user_timestamps = user_timestamps.where("date(convert_tz(created_at, '+00:00', '-10:00')) <= ?", filter.date_end) if filter.date_end.present?
+    user_timestamps = user_timestamps.where{self.path =~ "%#{filter.path}%"} if filter.path.present?
+  end
 end
