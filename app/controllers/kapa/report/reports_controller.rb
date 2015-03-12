e5d2e208fce8d1924d16dfe520560f431fa5b6ce
Marifel <mrbarbasa@gmail.com>
2015-03-12 12:33:31 -1000

    Implemented report/dataset parameters functionality part 4

--- app/controllers/kapa/report/reports_controller.rb
+++ app/controllers/kapa/report/reports_controller.rb
@@ -11,6 +11,8 @@ class Kapa::Report::ReportsController < Kapa::KapaBaseController
   def show
     @dataset = Dataset.find(params[:id])
     @dataset.load if @dataset.loaded_at.blank?
+    @parameters = @dataset.deserialize(:parameters, :as => OpenStruct)
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @dataset }
