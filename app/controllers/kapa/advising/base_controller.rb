6c1257c6035b294f655535d5391f84e8559f4dd4
Marifel Barbasa <mrbarbasa@gmail.com>
2015-04-24 10:38:03 -1000

    Used ActiveSupport::Concern in advising controllers

--- app/controllers/kapa/advising/base_controller.rb
+++ app/controllers/kapa/advising/base_controller.rb
@@ -1,7 +1,3 @@
 class Kapa::Advising::BaseController < Kapa::KapaBaseController
-
-  private
-  def filter_defaults
-    {:date_start => Date.today, :date_end => Date.today, :per_page => Rails.configuration.items_per_page}
-  end
+  include Kapa::Advising::Concerns::BaseController
 end
