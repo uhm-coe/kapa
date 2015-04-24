25e3c9a063cdd2c407bc9f2c259e35843899df8b
Marifel Barbasa <mrbarbasa@gmail.com>
2015-04-24 11:05:10 -1000

    Used ActiveSupport::Concern in course controllers

---
+++ app/controllers/kapa/course/concerns/base_controller.rb
@@ -0,0 +1,8 @@
+module Kapa::Course::Concerns::BaseController
+  extend ActiveSupport::Concern
+
+  private
+  def filter_defaults
+    {:term_id => Term.current_term.id, :per_page => Rails.configuration.items_per_page}
+  end
+end
