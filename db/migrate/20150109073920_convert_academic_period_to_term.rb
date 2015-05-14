be2f1e1114338181d5d3a66ddfa6cb7f24e49033
genta <genta@hawaii.edu>
2015-05-14 13:10:18 -1000

    Updated migration.

--- db/migrate/20150109073920_convert_academic_period_to_term.rb
+++ db/migrate/20150109073920_convert_academic_period_to_term.rb
@@ -3,7 +3,8 @@ class ConvertAcademicPeriodToTerm < ActiveRecord::Migration
     [Kapa::CourseOffer, Kapa::Form, Kapa::PracticumPlacement, Kapa::TransitionPoint].each do |t|
       t.update_all("term_id = (select id from terms where code = academic_period)")
     end
-    Kapa::AssessmentRubric.update_all("start_term_id = (select id from terms where code = academic_period_start), end_term_id = (select id from terms where code = academic_period_end)")
+    Kapa::AssessmentRubric.update_all("start_term_id = (select id from terms where code = academic_period_start)")
+    Kapa::AssessmentRubric.update_all("end_term_id = (select id from terms where code = academic_period_end)")
     Kapa::ProgramOffer.all.each do |o|
       term_ids = []
       o.available_academic_period.split(/,\s*/).each do |a|
