fe83ae3c7ecf07e7f53ce4a275d78ee8599a2035
genta <genta@hawaii.edu>
2015-08-03 23:44:27 -1000

    Fixed advising sessions issue.

--- lib/generators/kapa/cp/cp_generator.rb
+++ lib/generators/kapa/cp/cp_generator.rb
@@ -5,8 +5,6 @@ class Kapa::CpGenerator < Rails::Generators::NamedBase
   desc "Copy a file from Kapa::Engine to your application for customizing controllers, models, views, etc..."
 
   def copy_engine_file
-
     copy_file "#{Kapa::Engine.root}/#{file_path}", "#{Rails.root}/#{file_path}"
   end
-
 end
--- lib/generators/kapa/install/install_generator.rb
+++ lib/generators/kapa/install/install_generator.rb
@@ -5,27 +5,17 @@ class Kapa::InstallGenerator < Rails::Generators::Base
 
   def install_initializers
     puts "Installing initializers..."
-    install_file("#{Rails.root}/config/initializers/kapa.rb")
+    copy_file("kapa.rb", "#{Rails.root}/config/initializers/kapa.rb")
     inject_into_file "#{Rails.root}/config/initializers/mime_types.rb", "Mime::Type.register \"application/octet-stream\", :file\n", :after => "# Add new mime types for use in respond_to blocks:\n"
   end
 
   def install_seeds
     puts "Installing database seeds..."
-    install_file("#{Rails.root}/db/seeds.rb")
+    copy_file("#{Kapa::Engine.root}/db/seeds.rb", "#{Rails.root}/db/seeds.rb")
   end
 
   def install_fixtures
     puts "Installing fixtures..."
     directory("#{Kapa::Engine.root}/test/fixtures", "#{Rails.root}/test/fixtures")
-  end
-
-  private
-  def install_file(path)
-    if File.exists?(path)
-      puts "Skipping #{path} creation, as file already exists!"
-    else
-      puts "Installing a file (#{path})..."
-      copy_file(File.basename(path), path)
-    end
   end
 end
