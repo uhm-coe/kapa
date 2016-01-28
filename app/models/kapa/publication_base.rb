aaae02529072b58715a9d3a794f1696d55639740
Bryson McFeeley <brysonkm@hawaii.edu>
2016-01-28 11:24:59 -1000

    Add views for creating/updating publications

--- app/models/kapa/publication_base.rb
+++ app/models/kapa/publication_base.rb
@@ -3,6 +3,16 @@ module Kapa::PublicationBase
 
   included do
     belongs_to :person
+
     validates_presence_of :person_id
+    before_save :format_dates
+  end
+
+  def format_dates
+    unless self.pubdate.nil?
+      date = self.pubdate.to_time
+      self.pubyear = date.strftime("%Y")
+      self.pubmonth = date.strftime("%B")
+    end
   end
 end
