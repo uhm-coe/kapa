a45c3a5761544297887b791ad56abf16339c8706
Bryson McFeeley <brysonkm@hawaii.edu>
2016-01-27 14:49:56 -1000

    Implement basic CRUD methods for publications

--- app/controllers/kapa/publications_controller_base.rb
+++ app/controllers/kapa/publications_controller_base.rb
@@ -7,20 +7,57 @@ module Kapa::PublicationsControllerBase
   end
 
   def show
+    @publication = Kapa::Publication.find(params[:id])
+    @person = @publication.person
   end
 
   def new
+    @person = Kapa::Person.find(params[:id])
   end
 
   def create
+    @person = Kapa::Person.find(params[:id])
+    @publication = Kapa::Publication.new(publication_params)
+    @publication.attributes = publication_params
+    @publication.person = @person
+
+    unless @publication.save
+      flash[:danger] = error_message_for(@publication)
+      redirect_to new_kapa_publication_path(:id => @person) and return false
+    end
+
+    flash[:success] = "Publication was successfully created."
+    redirect_to kapa_publication_path(:id => @publication)
   end
 
   def update
+    @publication = Kapa::Publication.find(params[:id])
+    @publication.attributes = publication_params
+
+    unless @publication.save
+      flash[:danger] = @publication.errors.full_messages.join(", ")
+      redirect_to kapa_publication_path(:id => @publication) and return false
+    end
+
+    flash[:success] = "Publication was successfully updated."
+    redirect_to kapa_publication_path(:id => @publication)
   end
 
   def destroy
+    @publication = Kapa::Publication.find(params[:id])
+    unless @publication.destroy
+      flash[:danger] = error_message_for(@publication)
+      redirect_to kapa_publication_path(:id => @publication) and return false
+    end
+    flash[:success] = "Publication was successfully deleted."
+    redirect_to kapa_person_path(:id => @publication.person_id)
   end
 
   def export
+  end
+
+  private
+  def publication_params
+    params.require(:publication).permit(:person_id, :dept, :authors, :pubpages, :pubabstract, :pubdate, :publocation, :pubowner, :pubpublisher, :documentauthor, :pubtype, :pubkeyword, :pubvenue, :pubvol, :pubcreator, :pubtitle, :pubmonth, :pubyear, :documentidentifier, :pubcontributor, :pubeditor, :pubbooktitle, :pubthumbnail, :pubbookchapter, :pubisnotfeatured, :puborganization, :pubpdf, :pubedition, :documentlocation)
   end
 end
