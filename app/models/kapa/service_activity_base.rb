c3d7d8c7e3e702a6ad29dbfd7ae98e4adadd19d4
Bryson McFeeley <brysonkm@hawaii.edu>
2016-02-02 09:46:05 -1000

    Add search fields for service activities

--- app/models/kapa/service_activity_base.rb
+++ app/models/kapa/service_activity_base.rb
@@ -11,5 +11,15 @@ module Kapa::ServiceActivityBase
   end
 
   class_methods do
+    def search(options = {})
+      filter = options[:filter].is_a?(Hash) ? OpenStruct.new(options[:filter]) : options[:filter]
+      service_activities = Kapa::ServiceActivity.eager_load([:person]).order("persons.last_name, persons.first_name")
+      service_activities = service_activities.where("service_activities.name LIKE ?", "%#{filter.name}%") if filter.name.present?
+      service_activities = service_activities.where("service_activities.affiliation LIKE ?", "%#{filter.affiliation}%") if filter.affiliation.present?
+      service_activities = service_activities.where("service_activities.service_type" => filter.service_type) if filter.service_type.present?
+      service_activities = service_activities.where("service_activities.context" => filter.context) if filter.context.present?
+      service_activities = service_activities.where(:pubdate => filter.pubdate_start..filter.pubdate_end) if filter.pubdate_start.present? and filter.pubdate_end.present?
+      return service_activities
+    end
   end
 end
