06bf63e3bc9a324138174e5b254575b3b9ee5a40
genta <genta@hawaii.edu>
2016-03-15 17:14:45 -1000

    Added person_assignments.

--- app/controllers/kapa/person_assignments_controller_base.rb
+++ app/controllers/kapa/person_assignments_controller_base.rb
@@ -3,10 +3,9 @@ module Kapa::PersonAssignmentsControllerBase
 
   def show
     @person_assignment = Kapa::PersonAssignment.find(params[:id])
+    @person = @person_assignment.person
+    @contact =  @person.contact
     @assignable = @person_assignment.assignable
-    @person = @assignable.person
-    @assignee = @person_assignment.person
-    @assignee_contact =  @assignee.contact
   end
 
   def new
@@ -25,20 +24,20 @@ module Kapa::PersonAssignmentsControllerBase
 
   def update
     @person_assignment = Kapa::PersonAssignment.find(params[:id])
-    @assignee = @person_assignment.person
-    @assignee_contact =  @assignee.contact ||= @assignee.build_contact
+    @person = @person_assignment.person
+    @contact =  @person.contact
 
     @person_assignment.attributes = person_assignment_params
-    @assignee.attributes = assignee_params
-    @assignee_contact.attributes = assignee_contact_params
+    @person.attributes = person_params if params[:person]
+    @contact.attributes = contact_params if params[:contact]
 
-    unless @person_assignment.save and @assignee.save and @assignee_contact.save
-      flash[:danger] = error_message_for(@person_assignment, @assignee, @assignee_contact)
+    unless @person_assignment.save and @person.save and @contact.save
+      flash[:danger] = error_message_for(@person_assignment, @person, @contact)
       redirect_to kapa_assignment_path(:id => @person_assignment) and return false
     end
 
     flash[:success] = "Person was successfully updated."
-    redirect_to kapa_person_assignment_path(:id => @person_assignment)
+    redirect_to kapa_person_assignment_path(:id => @person_assignment, :return_path => params[:return_path])
   end
 
   def destroy
@@ -57,16 +56,16 @@ module Kapa::PersonAssignmentsControllerBase
 
   private
   def person_assignment_params
-    params.require(:person_assignment).permit(:assignable_id, :assignable_type, :type, :person_id, :sequence)
+    params.require(:person_assignment).permit(:assignable_id, :assignable_type, :type, :person_id, :note, :sequence)
   end
 
-  def assignee_params
-    params.require(:assignee).permit(:id_number, :last_name, :middle_initial, :birth_date, :ssn, :ssn_agreement,
+  def person_params
+    params.require(:person).permit(:id_number, :last_name, :middle_initial, :birth_date, :ssn, :ssn_agreement,
                                     :email, :first_name, :other_name, :title, :gender, :status)
   end
 
-  def assignee_contact_params
-    params.require(:assignee_contact).permit(:cur_phone, :mobile_phone, :email, :cur_street, :cur_city, :cur_state,
+  def contact_params
+    params.require(:contact).permit(:cur_phone, :mobile_phone, :email, :cur_street, :cur_city, :cur_state,
                                     :cur_postal_code, :per_street, :per_city, :per_state, :per_postal_code)
   end
 end
