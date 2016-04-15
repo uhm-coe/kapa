de15ea90de6d13ee65f64d32ac8df7491e37f99f
genta <genta@hawaii.edu>
2016-04-14 18:27:58 -1000

    Updated case form.

--- app/controllers/kapa/persons_controller_base.rb
+++ app/controllers/kapa/persons_controller_base.rb
@@ -11,7 +11,7 @@ module Kapa::PersonsControllerBase
     @documents += @person.exams
     @curriculums = @person.curriculums.eager_load(:transition_points => :term).order("terms.sequence DESC")
     @advising_sessions = @person.advising_sessions.order("session_date DESC")
-    @case_involvements = @person.case_involvements.eager_load(:case).order("cases.start_date DESC")
+    @case_involvements = @person.case_involvements.eager_load(:case).order("cases.reported_at DESC")
     @course_registrations = @person.course_registrations.eager_load(:course => :term).order("terms.sequence DESC")
     @practicum_placements = @person.practicum_placements
     @publications = @person.faculty_publications
