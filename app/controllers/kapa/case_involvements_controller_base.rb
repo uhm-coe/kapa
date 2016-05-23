7f5505065f1c04bef5be878e8351e4bd157d8a41
genta <genta@hawaii.edu>
2016-05-22 15:37:11 -1000

    Update case_involvement form.

--- app/controllers/kapa/case_involvements_controller_base.rb
+++ app/controllers/kapa/case_involvements_controller_base.rb
@@ -39,14 +39,6 @@ module Kapa::CaseInvolvementsControllerBase
     @case_involvement = Kapa::CaseInvolvement.find(params[:id])
     @case = @case_involvement.case
 
-    @person = @case_involvement.person
-    @person.attributes = person_params if params[:person]
-    @person.attributes = contact_params if params[:contact]
-    unless @person.save
-      flash[:danger] = error_message_for(@person)
-      redirect_to kapa_case_involvement_path(:id => @case_involvement) and return false
-    end
-
     @case_involvement.attributes = case_involvement_params
     unless @case_involvement.save and @case.save
       flash[:danger] = error_message_for(@case_involvement)
@@ -70,16 +62,6 @@ module Kapa::CaseInvolvementsControllerBase
 
   private
   def case_involvement_params
-    params.require(:case_involvement).permit(:case_id, :type, :person_id, :note, :sequence)
-  end
-
-  def person_params
-    params.require(:person).permit(:id_number, :last_name, :middle_initial, :birth_date, :ssn, :ssn_agreement,
-                                    :email, :first_name, :other_name, :title, :gender, :status)
-  end
-
-  def contact_params
-    params.require(:contact).permit(:entity_id, :entity_type, :cur_phone, :mobile_phone, :email, :cur_street, :cur_city, :cur_state,
-                                    :cur_postal_code, :per_street, :per_city, :per_state, :per_postal_code)
+    params.require(:case_involvement).permit(:case_id, :type, :affiliation, :bargaining_unit, :job_title, :person_id, :note, :sequence)
   end
 end
