619b0e2d7616f5dc11b9436eda75cd459039ad91
genta <genta@hawaii.edu>
2016-06-24 17:40:24 +0900

    Add witness and docuemnts selectors

--- app/controllers/kapa/case_incidents_controller_base.rb
+++ app/controllers/kapa/case_incidents_controller_base.rb
@@ -14,6 +14,7 @@ module Kapa::CaseIncidentsControllerBase
     @case = Kapa::Case.find(params[:case_id])
     @case_involvements = @case.case_involvements
     @case_incident = @case.case_incidents.build(:incident_occurred_at => DateTime.now)
+    @documents = @case.documents
   end
 
   def create
