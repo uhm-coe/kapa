12f4b2d02718680d8c36e7ffbb3c2a0db82f1c89
genta <genta@hawaii.edu>
2016-09-08 15:14:39 -1000

    Add bibtex fields.

---
+++ db/migrate/20160201022357_add_bibtex_fields.rb
@@ -0,0 +1,19 @@
+class AddBibtexFields < ActiveRecord::Migration
+  def change
+    change_table :faculty_publications, :bulk => true do |t|
+      t.string "category"
+      t.string "annote"
+      t.text "auther"
+      t.string "how_published"
+      t.string "journal"
+      t.string "key"
+      t.text "note"
+      t.string "issue_number"
+      t.string "school"
+      t.string "series"
+      t.string "crossref"
+      t.text "bibtex"
+      t.remove "num_of_vol"
+    end
+  end
+end
