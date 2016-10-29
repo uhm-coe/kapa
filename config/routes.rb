cf54f556351fdc66045e0988e34edba160839a94
genta <genta@hawaii.edu>
2016-10-28 18:33:09 -1000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  extra_actions = Proc.new do
+  concern :extra_actions do
     collection do
       get :export
       post :import
@@ -14,34 +14,34 @@ Rails.application.routes.draw do
     resource :user_session
     get 'persons/lookup(/:id)' => 'persons#lookup', :as => :persons_lookup
     get 'persons/sync(/:id)' => 'persons#sync', :as => :persons_sync
-    resources :persons, &extra_actions
-    resources :curriculums, &extra_actions
-    resources :transition_points, &extra_actions
+    resources :persons, :concerns => :extra_actions
+    resources :curriculums, :concerns => :extra_actions
+    resources :transition_points, :concerns => :extra_actions
     resources :transition_actions
-    resources :enrollments, &extra_actions
-    resources :files, &extra_actions
-    resources :forms, &extra_actions
-    resources :texts, &extra_actions
-    resources :text_templates, &extra_actions
-    resources :exams, &extra_actions
-    resources :reports, &extra_actions
-    resources :advising_sessions, &extra_actions
-    resources :courses, &extra_actions
-    resources :course_registrations, &extra_actions
+    resources :enrollments, :concerns => :extra_actions
+    resources :files, :concerns => :extra_actions
+    resources :forms, :concerns => :extra_actions
+    resources :texts, :concerns => :extra_actions
+    resources :text_templates, :concerns => :extra_actions
+    resources :exams, :concerns => :extra_actions
+    resources :reports, :concerns => :extra_actions
+    resources :advising_sessions, :concerns => :extra_actions
+    resources :courses, :concerns => :extra_actions
+    resources :course_registrations, :concerns => :extra_actions
     get 'practicum_placements/get_mentor(/:id)' => 'practicum_placements#get_mentor', :as => :practicum_placements_get_mentor
     put 'practicum_placements/update_mentor(/:id)' => 'practicum_placements#update_mentor', :as => :practicum_placements_update_mentor
-    resources :practicum_placements, &extra_actions
-    resources :practicum_logs, &extra_actions
-    resources :practicum_sites, &extra_actions
-    resources :users, &extra_actions
-    resources :user_assignments, &extra_actions
-    resources :terms, &extra_actions
-    resources :programs, &extra_actions
+    resources :practicum_placements, :concerns => :extra_actions
+    resources :practicum_logs, :concerns => :extra_actions
+    resources :practicum_sites, :concerns => :extra_actions
+    resources :users, :concerns => :extra_actions
+    resources :user_assignments, :concerns => :extra_actions
+    resources :terms, :concerns => :extra_actions
+    resources :programs, :concerns => :extra_actions
     resources :program_offers
-    resources :properties, &extra_actions
-    resources :assessment_rubrics, &extra_actions
-    resources :assessment_criterions, &extra_actions
-    resources :datasets, &extra_actions
+    resources :properties, :concerns => :extra_actions
+    resources :assessment_rubrics, :concerns => :extra_actions
+    resources :assessment_criterions, :concerns => :extra_actions
+    resources :datasets, :concerns => :extra_actions
     put 'datasets/load(/:id)' => 'datasets#load_data', :as => :datasets_load
     get 'datasets/feed(/:id)' => 'datasets#feed', :as => :datasets_feed
   end
