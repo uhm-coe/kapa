30023b7386f069b1006d14ed98ce512d7c039720
genta <genta@hawaii.edu>
2016-12-06 14:36:41 -1000

    Fix default config files.

---
+++ test/dummy/config/initializers/time_formats.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+Time::DATE_FORMATS[:default] = '%m/%d/%Y %I:%M %p'
+Time::DATE_FORMATS[:datetime] = '%m/%d/%Y %I:%M %p'
+Time::DATE_FORMATS[:date] = '%m/%d/%Y'
+Time::DATE_FORMATS[:time] = '%I:%M:%S %p'
+Date::DATE_FORMATS[:default] = '%m/%d/%Y'
