59302390061799c2cb2ad0ccccaafe0957a2c65e
genta <genta@hawaii.edu>
2017-04-26 12:35:02 -1000

    Move KAPA business controllers/models/views into kapa-coe project

--- config/routes.rb
+++ config/routes.rb
@@ -15,34 +15,18 @@ Rails.application.routes.draw do
     get 'persons/lookup(/:id)' => 'persons#lookup', :as => :persons_lookup
     get 'persons/sync(/:id)' => 'persons#sync', :as => :persons_sync
     resources :persons, &extra_actions
-    resources :curriculums, &extra_actions
-    resources :transition_points, &extra_actions
-    resources :transition_actions
-    resources :enrollments, &extra_actions
     resources :files, &extra_actions
     resources :forms, &extra_actions
     resources :texts, &extra_actions
     resources :text_templates, &extra_actions
-    resources :exams, &extra_actions
-    resources :reports, &extra_actions
-    resources :advising_sessions, &extra_actions
-    resources :courses, &extra_actions
-    resources :course_registrations, &extra_actions
-    get 'practicum_placements/get_mentor(/:id)' => 'practicum_placements#get_mentor', :as => :practicum_placements_get_mentor
-    put 'practicum_placements/update_mentor(/:id)' => 'practicum_placements#update_mentor', :as => :practicum_placements_update_mentor
-    resources :practicum_placements, &extra_actions
-    resources :practicum_logs, &extra_actions
-    resources :practicum_sites, &extra_actions
-    resources :users, &extra_actions
-    resources :user_assignments, &extra_actions
-    resources :terms, &extra_actions
-    resources :programs, &extra_actions
-    resources :program_offers
-    resources :properties, &extra_actions
     resources :assessment_rubrics, &extra_actions
     resources :assessment_criterions, &extra_actions
+    resources :properties, &extra_actions
+    resources :reports, &extra_actions
     put 'datasets/load(/:id)' => 'datasets#load_data', :as => :datasets_load
     get 'datasets/feed(/:id)' => 'datasets#feed', :as => :datasets_feed
+    resources :users, &extra_actions
+    resources :user_assignments, &extra_actions
   end
 
 end
