9064f4156408321b20846a9d59748b22d925946c
genta <genta@hawaii.edu>
2017-07-25 15:28:30 -1000

    Updated installer and readme file.

--- README.md
+++ README.md
@@ -1,28 +1,27 @@
 #KAPA
 
-KAPA provides consolidated information tools to organize student academic records including advising, courses,
-transition points, field placements, electronic documents, etc....  Tools are distributed as [Rails Engine](http://guides.rubyonrails.org/engines.html) so that developers can easily customize designs and functions to fit your organizational needs.
+KAPA provides consolidated information tools to organize office records including personal information, electronic documents, calendars, tasks, messaging/notifications, etc....  Tools are distributed as [Rails Engine](http://guides.rubyonrails.org/engines.html) so that developers can easily customize designs and functions to fit your organizational needs.
 
 ##Features
-- Demographic information
-- Academic programs and transition points (i.e., admission, qualifying exam, proposal defense, graduation)
-- Advising records
-- Course registrations
-- Field placements
-- Documents (file attachments, test scores, forms)
-- Assessments
+- Identity Management with LDAP support
+- Electronic Documents (file attachments, forms, and text documents)
+- User and Task Management
+- System Administration
+- Calendars and Events (Under development)
+- Messaging and Notifications (Under development)
+- Data Visualization (Under development)
 
 ### Getting Started with KAPA App
+
   1. Create a new Ruby on Rails Application.
   ```
-  rails new your_app
+  gem install rails -v 4.2.5 --no-rdoc --no-ri
+  rails new your_app -d mysql --skip
   ```
 
   12. Add following lines to your ```Gemfile```
   ```
-  gem 'kapa', :github => "https://github.com/uhm-coe/kapa.git"
-  gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git', :branch => 'bootstrap3'
-  gem 'rails-csv-fixtures', :git => 'https://github.com/bfolkens/rails-csv-fixtures.git'
+  gem 'kapa', :github => "uhm-coe/kapa"
   ```
   Don't forget to run ```bundle update``` to update your gems!
 
@@ -35,15 +34,16 @@ transition points, field placements, electronic documents, etc....  Tools are di
 
   3. Setup MySQL database
  
-  Current KAPA engine requires MySQL 5.5.  Update your config/database and run ```rake db:setup``` to generate database.
+  Current KAPA engine requires MySQL 5.5.  Update your config/database.yml and 
+  run ```rake kapa:db:setup``` to generate initial database schema.
 
-  4. Go to KAPA Admin Console to configure the application.
+  4. Go to KAPA Admin Console to start applocation development.
 
-  Open your browser and go to ```localhost:3000/kapa```.  
-  You can find default username and password in db/schema.rb
+  Open your browser and go to ```application_root/kapa```.
+  You can find default username and password in db/seed.rb
 
 ### Customizing KAPA App
-  Any code in KAPA Engine including controllers, models, and views can be modified/extended in your application.   We provide ```rails g kapa:cp``` command to copy engine source code to your application.
+  Any code in KAPA Engine including controllers, models, and views can be modified/extended in your application.   You can use ```rails g kapa:cp``` command to copy engine source code to your application.
 
   1. Customizing views
 
--- lib/generators/kapa/install/install_generator.rb
+++ lib/generators/kapa/install/install_generator.rb
@@ -5,55 +5,61 @@ class Kapa::InstallGenerator < Rails::Generators::Base
 
   def install_initializers
     puts "Installing initializers..."
-    copy_file("test/dummy/config/initializers/kapa.rb", "#{Rails.root}/config/initializers/kapa.rb")
-    copy_file("test/dummy/config/initializers/users.rb", "#{Rails.root}/config/initializers/users.rb")
-    copy_file("test/dummy/config/initializers/attachments.rb", "#{Rails.root}/config/initializers/attachments.rb")
-    copy_file("test/dummy/config/initializers/ldap.rb", "#{Rails.root}/config/initializers/ldap.rb")
-    copy_file("test/dummy/config/initializers/datasources.rb", "#{Rails.root}/config/initializers/datasources.rb")
-    copy_file("test/dummy/config/initializers/mailer.rb", "#{Rails.root}/config/initializers/mailer.rb")
+
+    copy_from_dummy("config/initializers/assets.rb")
+    copy_from_dummy("config/initializers/attachments.rb")
+    copy_from_dummy("config/initializers/cas.rb")
+    copy_from_dummy("config/initializers/datasources.rb")
+    copy_from_dummy("config/initializers/filter_parameter_logging.rb")
+    copy_from_dummy("config/initializers/kapa.rb")
+    copy_from_dummy("config/initializers/ldap.rb")
+    copy_from_dummy("config/initializers/mailer.rb")
+    copy_from_dummy("config/initializers/session_store.rb")
+    copy_from_dummy("config/initializers/time_formats.rb")
+    copy_from_dummy("config/initializers/users.rb")
     inject_into_file "#{Rails.root}/config/initializers/mime_types.rb", "Mime::Type.register \"application/octet-stream\", :file\n", :after => "# Add new mime types for use in respond_to blocks:\n"
   end
   
   def add_gem_dependencies
     puts "Adding gem dependencies..."
-    gem 'rails', '~> 4.2.3'
-    gem 'rails-csv-fixtures', :github => 'bfolkens/rails-csv-fixtures'
-    gem 'rake'
+    gem 'rails-csv-fixtures', github: 'bfolkens/rails-csv-fixtures'
     gem 'activerecord-session_store'
     gem 'authlogic', '~> 3.4.0'
     gem 'american_date'
     gem 'paperclip', '~> 3.0'
     gem 'fastercsv'
-    gem 'mysql2', '~> 0.3.18'
     gem 'net-ldap', '~> 0.13.0'
     gem 'sequel'
     gem 'will_paginate', '~> 3.0'
     gem 'will_paginate-bootstrap'
-    gem 'uglifier'
-    gem 'jquery-rails'
-    gem 'therubyracer'
     gem 'momentjs-rails', '>= 2.9.0'
-    gem 'sass-rails'
     gem 'bootstrap-sass'
     gem 'bootstrap3-datetimepicker-rails', '~> 4.14.30'
     gem 'bootstrap-multiselect-rails'
     gem 'dragula-rails'
     gem 'hashids'
     gem 'summernote-rails'
+    gem 'rails-assets-jquery', '1.12.4', source: 'https://rails-assets.org'
+    gem 'rails-assets-pivottable', source: 'https://rails-assets.org'
   end
 
   def install_locale
     puts "Installing default locale..."
-    copy_file("test/dummy/config/locales/en.yml", "#{Rails.root}/config/locales/en.yml")
+    copy_from_dummy("config/locales/en.yml")
   end
 
   def install_seeds
     puts "Installing database seeds..."
-    copy_file("test/dummy/db/seeds.rb", "#{Rails.root}/db/seeds.rb")
+    copy_from_dummy("db/seeds.rb")
   end
 
   def install_fixtures
     puts "Installing fixtures..."
     directory("test/fixtures", "#{Rails.root}/test/fixtures")
+  end
+
+  private
+  def copy_from_dummy(file_path)
+    copy_file("test/dummy/#{file_path}", "#{Rails.root}/#{file_path}")
   end
 end
