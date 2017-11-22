c7c6d5bb4b6629e8faf22c19ff53913e2c5f6f62
genta <genta@hawaii.edu>
2017-11-21 22:07:38 -1000

    Merge remote-tracking branch 'origin/release-1.7'
    Conflicts:
    	lib/kapa/engine.rb

--- test/dummy/config/initializers/attachments.rb
+++ test/dummy/config/initializers/attachments.rb
@@ -4,5 +4,8 @@
 #Location of attachment files
 Rails.configuration.attachment_root = Rails.env.production? ? "/srv/attachments" : "/home/ubuntu/attachments"
 
+#Allowed content types
+Rails.configuration.attachment_content_types = ["application/pdf", "application/x-pdf", "audio/mpeg", "image/gif", "image/jpeg", "image/png", "text/plain"]
+
 # Directory structure of attachment files
 Paperclip::Attachment.default_options[:path] = "#{Rails.configuration.attachment_root}/:class/:attachment/:id_partition/:style.:extension"
