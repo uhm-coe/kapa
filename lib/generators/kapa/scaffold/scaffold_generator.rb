6561b0c2f48648decc06a49533fb534c1622fbf4
genta <genta@hawaii.edu>
2018-02-23 21:07:23 -1000

    Add view templates

--- lib/generators/kapa/scaffold/scaffold_generator.rb
+++ lib/generators/kapa/scaffold/scaffold_generator.rb
@@ -12,10 +12,19 @@ class Kapa::ScaffoldGenerator < Rails::Generators::NamedBase
 
   def invoke_active_record_model
     Rails::Generators.invoke 'active_record:model', [file_path, "--no-test-framework", "--parent=Kapa::KapaModel" ]
+    module_file_path = File.join("app/models", class_path.join("/"))
+    remove_file(File.join("#{File.join("app/models", class_path.join("/"))}.rb"))
   end
 
   def copy_controller_file
-    template "controller.rb", File.join("app/controllers", "#{file_path}_controller.rb")
+    template "controller.rb", File.join("app/controllers", class_path.join("/"), "#{controller_name}_controller.rb")
+  end
+
+  def copy_view_files
+    template "new.html.erb", File.join("app/views", class_path.join("/"), controller_name, "new.html.erb")
+    template "show.html.erb", File.join("app/views", class_path.join("/"), controller_name, "show.html.erb")
+    template "index.html.erb", File.join("app/views", class_path.join("/"), controller_name, "index.html.erb")
+    template "form.html.erb", File.join("app/views", class_path.join("/"), controller_name, "_#{file_name}_form.html.erb")
   end
 
 end
