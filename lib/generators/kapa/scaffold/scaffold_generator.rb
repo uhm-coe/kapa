b05e87f435b0bcb63ff7b4c65afd2f01261a6344
genta <genta@hawaii.edu>
2018-02-27 12:05:47 -1000

    Update kapa:scaffold.

--- lib/generators/kapa/scaffold/scaffold_generator.rb
+++ lib/generators/kapa/scaffold/scaffold_generator.rb
@@ -10,21 +10,35 @@ class Kapa::ScaffoldGenerator < Rails::Generators::NamedBase
 
   source_root File.expand_path('../templates', __FILE__)
 
-  def invoke_active_record_model
+  def initialize(args = [], local_options = {}, config = {})
+    super
+    @model_options = args
+  end
+
+  def generate_model
+    @model_options[0] = "create_#{plural_name}"
+    @model_options << "yml:text"
+    @model_options << "json:text"
+    @model_options << "--timestamps"
+    Rails::Generators.invoke 'active_record:migration', @model_options
     Rails::Generators.invoke 'active_record:model', [file_path, "--no-test-framework", "--parent=Kapa::KapaModel" ]
-    module_file_path = File.join("app/models", class_path.join("/"))
     remove_file(File.join("#{File.join("app/models", class_path.join("/"))}.rb"))
   end
 
-  def copy_controller_file
-    template "controller.rb", File.join("app/controllers", class_path.join("/"), "#{controller_name}_controller.rb")
+  def generate_controller
+    template "controller.rb", File.join("app/controllers", "#{controller_name}_controller.rb")
   end
 
-  def copy_view_files
-    template "new.html.erb", File.join("app/views", class_path.join("/"), controller_name, "new.html.erb")
-    template "show.html.erb", File.join("app/views", class_path.join("/"), controller_name, "show.html.erb")
-    template "index.html.erb", File.join("app/views", class_path.join("/"), controller_name, "index.html.erb")
-    template "form.html.erb", File.join("app/views", class_path.join("/"), controller_name, "_#{file_name}_form.html.erb")
+  def generate_views
+    template "new.html.erb", File.join("app/views", controller_name, "new.html.erb")
+    template "show.html.erb", File.join("app/views", controller_name, "show.html.erb")
+    template "index.html.erb", File.join("app/views", controller_name, "index.html.erb")
+    template "form.html.erb", File.join("app/views", controller_name, "_#{file_name}_form.html.erb")
   end
 
+  def add_routes
+    Rails::Generators.invoke 'resource_route', [controller_name]
+    inject_into_file "#{Rails.root}/config/initializers/kapa.rb", "  #{plural_table_name}\n", :after => "Rails.configuration.available_routes = %w{\n"
+    puts "A new route was added.  Please restart to apply the new configration and run rake kapa:admin:user_role to give Admin users a permisison to access the new route."
+  end
 end
