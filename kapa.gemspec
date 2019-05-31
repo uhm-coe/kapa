2a7250e0fc3998ac24277b33d932edf006dd1bdc
genta <genta@hawaii.edu>
2019-05-30 15:39:45 -1000

    Update required libraries

--- kapa.gemspec
+++ kapa.gemspec
@@ -15,4 +15,21 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["LICENSE", "Rakefile", "README.md"]
   s.test_files = Dir["test/**/*"]
+
+  s.add_dependency 'rails', '~> 5.2.0'
+  s.add_dependency 'authlogic', '~> 4.4.2'
+  s.add_dependency 'american_date'
+  s.add_dependency 'paperclip'
+  s.add_dependency 'fastercsv'
+  s.add_dependency 'net-ldap'
+  s.add_dependency 'sequel'
+  s.add_dependency 'hashids'
+  s.add_dependency 'will_paginate'
+  s.add_dependency 'will_paginate-bootstrap'
+  s.add_dependency 'uglifier'
+  s.add_dependency 'sass-rails'
+  s.add_dependency 'bootstrap-sass'
+  s.add_dependency 'therubyracer'
+  s.add_dependency 'ckeditor'
+
 end
--- lib/generators/kapa/install/install_generator.rb
+++ lib/generators/kapa/install/install_generator.rb
@@ -13,7 +13,6 @@ class Kapa::InstallGenerator < Rails::Generators::Base
     copy_from_dummy("config/initializers/kapa.rb")
     copy_from_dummy("config/initializers/ldap.rb")
     copy_from_dummy("config/initializers/mailer.rb")
-    copy_from_dummy("config/initializers/session_store.rb")
     copy_from_dummy("config/initializers/time_formats.rb")
     copy_from_dummy("config/initializers/users.rb")
     inject_into_file "#{Rails.root}/config/initializers/mime_types.rb", "Mime::Type.register \"application/octet-stream\", :file\n", :after => "# Add new mime types for use in respond_to blocks:\n"
@@ -22,28 +21,22 @@ class Kapa::InstallGenerator < Rails::Generators::Base
   def add_gem_dependencies
     puts "Adding gem dependencies..."
     gem 'rails-csv-fixtures', :github => 'felixbuenemann/rails-csv-fixtures', :branch => 'rails-5.1-support'
-    gem 'rake'
     gem 'activerecord-session_store'
-    gem 'authlogic'
-    gem 'american_date'
-    gem 'paperclip'
-    gem 'fastercsv'
-    gem 'mysql2'
-    gem 'net-ldap'
-    gem 'sequel'
-    gem 'hashids'
-    gem 'will_paginate'
-    gem 'will_paginate-bootstrap'
-    gem 'uglifier'
-    gem 'therubyracer'
-    gem 'sass-rails'
-    gem 'bootstrap-sass'
-    gem 'ckeditor'
+    gem 'whenever', :require => false
   end
 
   def install_yarn_dependencies
     puts "Installing yarn dependencies..."
-    copy_file("package.json", "#{Rails.root}/package.json")
+  #  copy_file("package.json", "#{Rails.root}/package.json")
+    run "yarn add jquery"
+    run "yarn add rails-ujs"
+    run "yarn add dragula"
+    run "yarn add pivottable"
+    run "yarn add fullcalendar"
+    run "yarn add bootstrap-multiselect"
+    run "yarn add eonasdan-bootstrap-datetimepicker"
+    run "yarn add summernote"
+    run "yarn add codemirror"
   end
 
   def install_locale
