281992b27bd739e318d5696c322097e48fa5ac44
genta <genta@hawaii.edu>
2019-10-28 20:56:51 -1000

    Make the catch all route configurable

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,5 @@ Rails.application.routes.draw do
   end
 
   #catch-all route for 404 requests
-  match '*path', via: :all, to: 'application#error_404'
+  match '*path', via: :all, to: 'application#error_404' if Rails.configuration.try(:catch_all_route)
 end
