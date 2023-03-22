c2473794684ea63480e927fb38419cb54642f086
genta <genta@hawaii.edu>
2023-03-21 15:45:33 -1000

    Fix serialize_field= error

--- app/models/kapa/form_template_field_base.rb
+++ app/models/kapa/form_template_field_base.rb
@@ -7,7 +7,6 @@ module Kapa::FormTemplateFieldBase
     validates_uniqueness_of :name, :scope => :form_template_id
     validates_presence_of :label
     before_save :format_fields
-    self.serialize_field = :json
   end
 
   def format_fields
