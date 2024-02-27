a253d09b52b8f1cd34cd212253586e40ef616811
“genta” <genta@hawaii.edu>
2024-02-27 13:26:03 -1000

    Add catch-all route

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,8 @@ Rails.application.routes.draw do
     resources :user_assignments, &extra_actions
   end
 
-
+  #catch-all route for 404 (not found) requests
+  match '*path', via: :all, to: 'application#error_404' , constraints: lambda { |request|
+    !request.path.match? /^(\/rails)/
+  }
 end
