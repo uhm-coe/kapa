0153a890906d3c9e04b8822d9912965576fc8bd0
Genta T. <genta@hawaii.edu>
2026-06-24 15:47:46 +0900

    Update migration

--- db/migrate/20220414230808_add_active_fields.rb
+++ db/migrate/20220414230808_add_active_fields.rb
@@ -1,9 +1,9 @@
 class AddActiveFields < ActiveRecord::Migration[5.2]
 
   def change
-    add_column(:forms, :active, :boolean, :default => 1)
-    add_column(:files, :active, :boolean, :default => 1)
-    add_column(:texts, :active, :boolean, :default => 1)
+    add_column(:forms, :active, :boolean, :default => 1) unless column_exists?(:forms, :active)
+    add_column(:files, :active, :boolean, :default => 1) unless column_exists?(:files, :active)
+    add_column(:texts, :active, :boolean, :default => 1) unless column_exists?(:texts, :active)
   end
 
 end
